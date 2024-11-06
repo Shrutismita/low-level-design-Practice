d46ae4b67fdcf50ba8f7481dc5525ed790e92a1f
Shrutismita <k.s.sruti@gmail.com>
2024-11-07 00:55:11 +0530

    Create PermissionUserImp.java

---
+++ GoogleDoc/impl/PermissionUserImp.java
@@ -0,0 +1,10 @@
+package designGoogleDoc.impl;
+
+public class PermissionUserImp implements PermissionUser{
+
+    @Override
+    public void permissionUser() {
+        System.out.println("Permitted User is accessing the document.");
+    }
+
+}
